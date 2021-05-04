b2caef03e3ba8e87fb0db29769c68186ca233c9a
LJlkdskdjflsa <liliangjya@gmail.com>
2021-05-04 16:18:34 +0800

    user post with user id

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -31,8 +32,32 @@ class User extends Authenticatable
     ];
 
     //relation
-    public function posts(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
+    }
+    public function commands(): HasMany
+    {
+        return $this->hasMany(Command::class);
+    }
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    // function
+    public function getAvatar(){
+/*
+        $firstCharacter = $this->email[0];
+        if (is_numeric($firstCharacter)) {
+            $integerToUse = ord(strtolower($firstCharacter)) - 21;
+        } else {
+            $integerToUse = ord(strtolower($firstCharacter)) - 96;
+        }
+        return 'https://s.gravatar.com/avatar/'.md5($this->email)
+            .'?s=200'
+            .'&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+            .$integerToUse
+            .'.png';*/
     }
 }
